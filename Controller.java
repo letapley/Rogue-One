bffe1fd0db83fe0907a7fc74dc0c04ac51f313c0
layers1995 <layers1995@gmail.com>
2017-03-04 17:20:24 -0600

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,45 +1,46 @@
-
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-
-public class Controller {
+public class Controller implements KeyListener {
+		
+	@Override
+	public void keyReleased(KeyEvent e) {		
+	}
 	
-public void keyPressed(KeyEvent e) {
-
-	int key = e.getKeyCode();
-
-    if (key == KeyEvent.VK_H) {
-    	int[] left = {-1,0};
-    	Level.moveUnit(left);
-
-    }
-          
-
-    if (key == KeyEvent.VK_L) {
-        int[] right = {1,0};
-        Level.moveUnit(right);
-    }
-
-    if (key == KeyEvent.VK_J) {
-        int[] up = {0,1};
-        Level.moveUnit(up);
-    }
-
-    if (key == KeyEvent.VK_K) {
-        int[] down = {0,-1};
-        Level.moveUnit(down);
-    }
-
-}
+	@Override
+	public void keyPressed (KeyEvent e) {
+		int key = e.getKeyCode();
+		
+	    if (key == KeyEvent.VK_H) {
+	    	int[] left = {-1,0};
+	    	System.out.println("left");
+	    	//Level.moveUnit(left);
+	    }         
 	
-public void keyReleased(KeyEvent e) {
+	    if (key == KeyEvent.VK_L) {
+	        int[] right = {1,0};
+	        System.out.println("right");
+	        //Level.moveUnit(right);
+	    }
 	
-}
-
-public void keyTyped(KeyEvent e) {
+	    if (key == KeyEvent.VK_J) {
+	        int[] up = {0,-1};
+	        System.out.println("up");
+	        //Level.moveUnit(up);
+	    }
+	
+	    if (key == KeyEvent.VK_K) {
+	        int[] down = {0,1};
+	        System.out.println("down");
+	        //Level.moveUnit(down);
+	    }
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
 	
 }
